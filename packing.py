60095912cbd0c295755cf8df36ed4319311ec75c
len <l.yoshida.132@gmail.com>
2020-05-29 22:28:24 +0900

    write comment

--- packing.py
+++ packing.py
@@ -1,8 +1,26 @@
+def pack1(n, endianness = 'little'):
+    '''
+    return 8 bit
+    '''
+    return n.to_bytes(1,endianness)
+
 def pack2(n, endianness = 'little'):
+    '''
+    return 16 bit
+    '''
     return n.to_bytes(2,endianness)
 
 def pack4(n, endianness = 'little'):
+    '''
+    return 32 bit
+    '''
     return n.to_bytes(4,endianness)
+
+def pack8(n, endianness = 'little'):
+    '''
+    return 64 bit
+    '''
+    return n.to_bytes(8,endianness)
 
 def pack(n, endianness = 'little'):
     return pack4(n, endianness)
