24f6b919edb854e7c4d58d808c59e6cd544fc190
len <l.yoshida.132@gmail.com>
2020-05-30 14:48:56 +0900

    modified directory structure

--- qool/packing.py
+++ qool/packing.py
@@ -1,24 +1,24 @@
 def pack1(n, endianness = 'little'):
     '''
-    return 8 bit
+    8 bit
     '''
     return n.to_bytes(1,endianness)
 
 def pack2(n, endianness = 'little'):
     '''
-    return 16 bit
+    16 bit
     '''
     return n.to_bytes(2,endianness)
 
 def pack4(n, endianness = 'little'):
     '''
-    return 32 bit
+    32 bit
     '''
     return n.to_bytes(4,endianness)
 
 def pack8(n, endianness = 'little'):
     '''
-    return 64 bit
+    64 bit
     '''
     return n.to_bytes(8,endianness)
 
